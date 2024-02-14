f7973b6cac4bfb630728a0ebbe47f664cadf4291
SamNdungu <samwainaina087@gmail.com>
2024-02-14 23:05:36 +0300

    Edited intro.py file

--- lectures/course1/intro.py
+++ lectures/course1/intro.py
@@ -1,14 +1,15 @@
 # Addition, subtraction 
-print(5 + 5)  10 
-print(5 - 5)  0 
+print(5 + 5) 
+print(5 - 5) 
  
 # Multiplication, division, modulo, and exponentiation 
-print(3 * 5)  15 
-print(10 / 2)  5 
-print(18 % 7)  4 
-print(4 ** 2)  16 
- 
-# How much is your $100 worth after 7 years? 
-print(100 * 1.1 ** 7 )  194.87171 
+print(3 * 5) 
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2) 
+
+#  How much is your $100 worth after 7 years? 
+print(100 * 1.1 ** 7 )
+
  
   
